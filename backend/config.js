36fb15fc8b54a7c662c10ecb61ae6e6e3ea0db0e
Aleksandr Gribanov <agribanov@users.noreply.github.com>
2014-08-07 00:46:36 +0300

    socke session ready

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	port: process.env.PORT || 5000,
+	host: process.env.HOST || 'localhost',
+	dbURL: process.env.MONGOHQ_URL || "localhost/test",
+	sessionSecret: 'ilovescotchscotchyscotchscotch',
+	sidKey: "_sid"
+}
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	port: process.env.PORT || 5000,
-	host: process.env.HOST || 'localhost',
-	dbURL: process.env.MONGOHQ_URL || "localhost/test"
-}
